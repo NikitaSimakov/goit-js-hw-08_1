8f630b1e5b99d66aa287522c7a06f336b5a83fad
NikitaSimakov <mishaaniskin@gmail.com>
2023-03-04 11:29:17 +0300

    Second commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,35 @@
 // Add imports above this line
+import basicLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
 
-console.log('Hello!!');
+const listRef = document.querySelector('.gallery');
+const galleryMarkup = getMarkupGallery(galleryItems).join('');
+
+function getMarkupGallery(items) {
+  return items.map(
+    ({
+      preview,
+      original,
+      description,
+    }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" /></a>`
+  );
+}
+
+listRef.innerHTML += galleryMarkup;
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captions: true,
+  scrollZoom: false,
+  showCounter: false,
+});
+
+const sqrt = Math.sqrt;
+export const square = x => x * x;
+export const diag = (x, y) => sqrt(square(x) + square(y));
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
-console.log('This is vimeo page')
+import Player from '@vimeo/player';
+
+// const savedTime = 0;
+const handstickPlayer = new Player('vimeo-player');
+handstickPlayer.on('timeupdate', onTimeUpdate);
+
+function onTimeUpdate(event) {
+  //   savedTime = event.seconds;
+  console.log(event.seconds);
+  savedTime = event.seconds;
+  console.log(savedTime);
+}
+
+// import Player from '@vimeo/player';
+
+// const player = new Player('handstick', {
+//     id: 19231868,
+//     width: 640
+// });
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+
+import { square, diag } from './01-gallery';
+
+console.log(square(11)); // 121
+console.log(diag(4, 3)); // 5
