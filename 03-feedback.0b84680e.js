a7fe98d6e2de307ffc786736a5ad91396bb43bbc
NikitaSimakov <106473909+NikitaSimakov@users.noreply.github.com>
2023-03-06 11:40:34 +0000

    Deploying to gh-pages from @ NikitaSimakov/goit-js-hw-08_1@d292ae5edf1a4b4fb821b39e9d96c294e7526f78 🚀

--- 03-feedback.0b84680e.js
+++
@@ -1,2 +0,0 @@
-console.log("Page of feedback");
-//# sourceMappingURL=03-feedback.0b84680e.js.map
--- 03-feedback.ae238d98.js
+++
@@ -1,2 +0,0 @@
-console.log("Page of feedback");
-//# sourceMappingURL=03-feedback.ae238d98.js.map
